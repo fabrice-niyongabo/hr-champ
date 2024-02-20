b1bc86245ddebaaafc6a34aca650a330eb95f5a4
Fabrice Niiyongabo <ninga.fab@gmail.com>
2024-02-21 01:39:14 +0200

    displaying user messages

--- app/dashboard/jobs/new/description/message/index.tsx
+++ app/dashboard/jobs/new/description/message/index.tsx
@@ -8,9 +8,15 @@ interface IProps {
 function Message({ type, message }: IProps) {
   return (
     <div className="mb-3">
-      {type === "assistant" && (
-        <div className="bg-gray-300 p-3 inline-block max-w-[80%] rounded-xl rounded-bl-[0] text-sm">
+      {type === "assistant" ? (
+        <div className="bg-gray-300 p-3 inline-block max-w-[80%] rounded-3xl rounded-bl-[0] text-sm">
           {parse(message)}
+        </div>
+      ) : (
+        <div className="flex items-end justify-end">
+          <div className="bg-blue-600 p-3 inline-block max-w-[80%] rounded-3xl rounded-br-[0] text-sm text-white">
+            {parse(message)}
+          </div>
         </div>
       )}
     </div>
